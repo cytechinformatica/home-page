04c0a8e87e8ee3c8d478d7a82113c89337c96cbe
Felipe <felipe76857685@gmail.com>
2017-06-12 08:34:22 -0300

    Adicionando seções

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,23 @@
 $(() => {
 
+    $('.cy-btn-toggle-text').each((_, el) => {
+        $(el).click(e => {
+            e.preventDefault()
+            const toExpand = $(`${$(el).data('to-expand')}`)
+            const height = toExpand.find('p').outerHeight(true)
+            if($(el).data('collapsed')) {
+                toExpand.css({ height: 0 })
+                $(el).data('collapsed', false)
+                $(el).text($(el).data('collapsed-text'))
+            } else {
+                toExpand.css({ height: height })
+                $(el).data('collapsed', true)
+                $(el).data('collapsed-text', $(el).text())
+                $(el).text($(el).data('expanded-text'))
+            }
+        })
+    }) 
+
     $('.scrollspy').scrollSpy({ scrollOffset: 0 });
 
 })
--- app/scripts/toolbar.js
+++ app/scripts/toolbar.js
@@ -2,11 +2,100 @@ $(() => {
 
     const toolbarComponent = $('.cy-toolbar')
     const headerFabComponent = $('.cy-header-fab')
-    const firstSectionComponent = $('#1st-section')
+    const firstSectionComponent = $('.1st-section')
+
+    /**
+     * MENUS
+     */
+
+    const wavesCount = 2
+    const waves = []
+    const waveTemplate = index =>
+        (`<div id="cy-toolbar-active-circle-${index}" class="cy-toolbar-active-circle"></div>`)
+
+    const state = {
+        menu: -1,
+        menus: [],
+    }
+
+    $('.cy-toolbar-menu').each((i, el) => {
+        state.menus.push($(el))
+        $(el).click(() => goToMenu(i))
+    })
+
+    const updateWaves = (index = state.menu) => {
+        for (let i = 0; i < wavesCount; i++) {
+            let wave = $(`#cy-toolbar-active-circle-${i}`)
+            if (wave.length === 0) {
+                wave = $($.parseHTML(waveTemplate(i)))
+                wave.css({ transition: `all ${(i + 1) + 5}00ms cubic-bezier(0.8, 0, 0.2, 1)` })
+                toolbarComponent.append(wave)
+                waves.push(wave)
+            }
+        }
+
+        const menu = state.menus[index]
+
+        if (menu) {
+            const menuPos = menu.position()
+
+            let size = menu.height() / 1.15
+            let factor = 1.618
+            waves.map((w, i) => {
+                size *= factor
+                w.css({
+                    top: menuPos.top + menu.height() / 2 - size / 2,
+                    left: menuPos.left + menu.width() / 2 - size / 2,
+                    width: size,
+                    height: size
+                })
+            })
+        } else {
+            waves.map(w => w.css({
+                top: 0,
+                left: '50%',
+                width: 0,
+                height: 0
+            }))
+        }
+    }
+
+    const goToMenu = (index) => {
+        state.menu = index
+        updateWaves(index)
+    }
+
+    state.menus.map((m, i) => {
+        const sectionId = m.attr('href').replace('#', '')
+        const element = document.getElementById(sectionId)
+
+        if(element) {
+            new Waypoint({
+                element: document.getElementById(sectionId),
+                handler: function (direction) {
+                    if (direction == 'down') {
+                        goToMenu(i)
+                    } else {
+                        goToMenu(i - 1)
+                    }
+                },
+                offset: 100
+            })
+        }
+
+    })
+
+    /**
+     * END-MENUS
+     */
+
+    /**
+     * TOOLBAR ANIM
+     */
 
     $(window).scroll(() => {
         let windowScroll = $(window).scrollTop()
-        if(windowScroll >= $(window).height() - 52) {
+        if (windowScroll >= $(window).height() - 52) {
             toolbarComponent.removeClass('cy-toolbar-expanded')
             headerFabComponent.addClass('cy-hided')
             firstSectionComponent.css({ marginTop: 160 })
@@ -20,21 +109,11 @@ $(() => {
 
         const factor = (80 - Math.min(windowScroll, 80))
         toolbarComponent.css({ height: 80 + factor })
+
+        /**
+         * UPDATING MENU
+         */
+        updateWaves()
     })
-
-    
-    /**
-     * MENUS
-     */
-
-    const circleTemplate = index => 
-        (`<div id="cy-toolbar-active-circle-${index}" class="cy-toolbar-active-circle-${index}"></div>`)
-    
-    const state = {
-        menu: -1,
-        menus: [],
-    }
-
-     $('.cy-toolbar-menu').each((_, el) => state.menus.push($(el)))
 
 })
